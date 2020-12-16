4a5e91116f9766d474adddf841879b67bd9b8d87
Anthony Sgro <64649626+AnthonySgro@users.noreply.github.com>
2020-12-16 11:57:35 -0500

    Delete Multiples3Or5.py

--- Multiples3Or5.py
+++
@@ -1,8 +0,0 @@
-def solution(number):
-    solutions = []
-    for num in range(0, number):
-        if num % 3 == 0:
-            solutions.append(num)
-        elif num % 5 == 0:
-            solutions.append(num)
-    return sum(solutions)
